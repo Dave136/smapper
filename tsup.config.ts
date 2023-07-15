90de3926cac65dc7b5a43c19dd3c1af1d95e49a4
David Arenas <davejs136@gmail.com>
2023-07-14 23:37:05 -0400

    fix: declare and import types correctly

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   outDir: './dist',
   format: ['cjs', 'esm'],
-  dts: './src/types.ts',
+  dts: true,
   clean: true,
   bundle: true,
   minify: true,
