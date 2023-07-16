99ca165c4cf2fac494c9c333d23ab4c56dff4577
David Arenas <davejs136@gmail.com>
2023-07-16 11:56:07 -0400

    fix: no specify lodash as dependency

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   entry: ['src/index.ts'],
   outDir: './dist',
   format: ['cjs', 'esm'],
+  target: 'es6',
   dts: true,
   clean: true,
   bundle: true,
   minify: true,
+  noExternal: ['lodash'],
 });
